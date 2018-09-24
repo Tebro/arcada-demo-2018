9336036164c4f772d027c99b88d2c274363ceca0
Richard Weber <riche.weber@gmail.com>
2018-09-24 15:47:15 +0300

    Fixed cypress test

--- cypress/integration/frontpage_spec.js
+++ cypress/integration/frontpage_spec.js
@@ -4,6 +4,6 @@ const BASE_URL = 'http://localhost:3000';
 describe('Frontpage', () => {
   it('Opens up', () => {
     cy.visit(BASE_URL);
-    cy.contains('Hello World');
+    cy.contains('Hello');
   });
 });
